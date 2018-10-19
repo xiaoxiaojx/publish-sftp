94c4dcdd62d46d695c17745ae45e9f0339dace8b
xiaojiaxin6 <xiaojiaxin@jd.com>
2018-10-19 10:57:00 +0800

    fix size compute

--- copy.js
+++ copy.js
@@ -4,13 +4,15 @@ const { asyncForEach, checkRemotePath } = require("./utils");
 
 module.exports = function(options) {
   const { filesInfo, sftpConfig, sftp } = options;
-  const total = filesInfo.length;
-  const size = filesInfo.reduce((preVal, cVal) => preVal + cVal.size, 0);
+  const currentFilesInfo = filesInfo.filter(info => !info.isDirectory);
+  const total = currentFilesInfo.length;
+  const size = currentFilesInfo.reduce((preVal, cVal) => preVal + cVal.size, 0);
   const spinner = ora("开始传输文件... \n").start();
+  let currentSize = 0
 
   ora().info(`总共 ${total} 个文件, 大小总计 ${size} 字节 需要传输 ... \n`);
 
-  asyncForEach(filesInfo, (info, index, array) => {
+  asyncForEach(filesInfo, info => {
     const absBasePath = path.join(process.cwd(), sftpConfig.localPath);
     const absLocalPath = path.join(process.cwd(), info.path);
     const absRemotePath = path
@@ -26,10 +28,11 @@ module.exports = function(options) {
       return sftp.mkdir(absRemotePath, true);
     } else {
       sftp.fastPut(absLocalPath, absRemotePath).then(() => {
-        spinner.text = `传输: ${((index * 100) / array.length).toFixed(
+        currentSize += info.size
+        spinner.text = `传输: ${((currentSize * 100) / size).toFixed(
           2
-        )}% ${absRemotePath}`;
-        if (index === array.length - 1) {
+        )}% ${absRemotePath} ${info.size} 字节`;
+        if (currentSize === size) {
           spinner.succeed("传输: 100%");
           sftp.end();
         }
